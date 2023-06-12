9310c1618b00fedafd0e7908493b7fd5993497ee
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-06-12 11:31:42 -0700

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, sub, mul, div
+    def handle_operation(a, operator, b):
+        if operator == "+":
+            result = add(a, b)
+            op_symb = "+"
+        elif operator == "*":
+            result = mul(a, b)
+            op_symb = "*"
+        elif operator == "/":
+            result = div(a, b)
+            op_symb = "/"
+        elif operator == "-":
+            result = sub(a, b)
+            op_symb = "-"
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        print("{} {} {} = {}".format(a, op_symb, b, result))
 
-def handle_operation(a, operator, b):
-    if operator == "+":
-        result = add(a, b)
-        op_symb = "+"
-    elif operator == "*":
-        result = mul(a, b)
-        op_symb = "*"
-    elif operator == "/":
-        result = div(a,b)
-        op_symb = "/"
-    elif operator == "-":
-        result = sub(a, b)
-        op_symb = "-"
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    print("{} {} {} = {}".format(a, op_symb, b, result))
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
-
-handle_operation(a, operator, b)
+    handle_operation(a, operator, b)
