da8f102aaa8fd2215e62103938f381a48e2b71c8
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-06-26 12:02:58 -0700

    done

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+	for elemeng in my_list:
+            print(element, end=' ')
+            count += 1
+	    if count == x:
+                break
+            else:
+	        raise IndexError
+    except IndexError:
+        pass
+    finally:
+	print()
+    return (count)
