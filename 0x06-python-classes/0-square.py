45986be3032b4e9eb81816217b58de2d9479df1d
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-06-27 14:29:29 -0700

    done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Represents a square shape.
+
+    This class provides a basic template for creating square objects.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Usage:
+        square = Square()
+    """
     pass
