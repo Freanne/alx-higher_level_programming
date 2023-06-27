2d8b52044368e676badeec77e67c0e5c7f5a9bbe
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-06-27 14:46:45 -0700

    do,e

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+""" Define a class Square """
+
+
 class Square:
     def __init__(self, __size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+        """
         self.__size = __size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
+""" Define a class Square """
+
+
 class Square:
+    """
+
+        This class represents a square.
+
+        Attributes:
+        __size (int): The size of the square.
+
+    """
     def __init__(self, size=0):
+        """
+
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
