356bb42ca223aa922ff5dc84518879670d02c40d
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-07-24 10:02:42 -0700

    Done

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines class square inherit Rectangle class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ implements the class """
+    def __init__(self, size):
+        """ 
+            size(int) : must be private.
+            size: must be positive number validate by integer validator
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """ return area of square """
+        return self.size ** 2
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines class square inherit Rectangle class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ implements the class """
+    def __init__(self, size):
+        """ 
+            size(int) : must be private.
+            size: must be positive number validate by integer validator
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """ return area of square """
+        return self.size ** 2
+
+    def __str__(self):
+        """ represent print and str """
+        string = "[" + str(self.__class__.__name__) + "]"
+        string += str(self.__size) + "/" str(self.__size)
+        return string
