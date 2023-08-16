a714af43690ab5d5c4cf103592a984ca8651384b
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-08-16 02:21:52 -0700

    done create table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,8 @@
---create tables with a script
+--create a table in script SQL in the current database
+-- passed the name of database in arguments
 
-
+CREATE TABLE IF NOT EXISTS first_table
+(
+	id INT,
+	name VARCHAR(256)
+);
