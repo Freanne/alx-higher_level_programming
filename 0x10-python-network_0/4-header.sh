6994f7c24ed47e76f30dfdc24f8ea05b1e9d41c0
Freanne <104914160+Freanne@users.noreply.github.com>
2023-12-05 23:29:49 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Check if the number of arguments is correct
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
 # Get the URL from the command line argument
 url=$1
 
