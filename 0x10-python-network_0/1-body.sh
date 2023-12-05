06b500594786cba949b7b813b69f578568686133
Freanne <104914160+Freanne@users.noreply.github.com>
2023-12-05 23:26:41 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Check if the number of arguments is correct
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url=$1
-
-# Use curl to send a GET request and store the response in a variable
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-# Check if the response status code is 200
-if [ "$response" -eq 200 ]; then
-    # If the status code is 200, display the body of the response
-    curl -s "$url"
-fi
+# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -X GET -sL "$1"
