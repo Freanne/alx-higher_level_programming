f237dc69185ce311d834eee71d9e955bfda46310
Freanne <104914160+Freanne@users.noreply.github.com>
2023-12-05 23:31:26 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Check if the number of arguments is correct
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
 # Get the URL from the command line argument
 url=$1
 
@@ -14,4 +7,4 @@ email="test@gmail.com"
 subject="I will always be here for PLD"
 
 # Use curl to send a POST request with the specified parameters and display the body of the response
-curl -s -X POST -d "email=$email&subject=$subject" "$url"
+curl -X POST "$url"-s -d "email=$email&subject=$subject"
