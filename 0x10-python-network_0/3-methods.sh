ab9f1bcae288fbd9f0e724047f445e4d1c1c66c4
Freanne <104914160+Freanne@users.noreply.github.com>
2023-12-05 23:29:13 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 # Check if the number of arguments is correct
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url=$1
-
-# Use curl to send an OPTIONS request and display the Allow header from the response
-curl -sI -X OPTIONS "$url" | grep -i "Allow" | awk '{print $2}'
+curl -sI "$1" | grep Allow | awk '{split($0, methods, ": "); print methods[2]}'
