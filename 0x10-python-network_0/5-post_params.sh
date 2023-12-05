a26b4e3729ef5e21e9e640c308a8d81b31b5b3f1
Freanne <104914160+Freanne@users.noreply.github.com>
2023-12-05 23:35:48 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Get the URL from the command line argument
-url=$1
-
-# Define the POST parameters
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Use curl to send a POST request with the specified parameters and display the body of the response
-curl -X POST "$url"-s -d "email=$email&subject=$subject"
+# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -X POST "$1" -s -d 'email=test@gmail.com&subject=I will always be here for PLD'
